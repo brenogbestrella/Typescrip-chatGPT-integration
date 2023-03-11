2a55c62dd214da61b99e66a32ff067f04e8db375
Breno Estrella <brenogbestrella@gmail.com>
2023-03-11 17:32:11 -0300

    Changed the logic on how to get the tag; changed the openAI model used

--- Backend/services/HomeService.ts
+++ Backend/services/HomeService.ts
@@ -1,4 +1,4 @@
-import cheerio, { AnyNode } from "cheerio";
+import cheerio from "cheerio";
 import axios from "axios";
 require('dotenv').config;
 
@@ -8,12 +8,30 @@ async function getTextFromWebPage(urlObj: {url: string}): Promise<string> {
         const response = await axios.get(urlObj.url);
         const html = response.data;
         const $ = cheerio.load(html);
-        const text = $('article').text();
+
+        let maxNumberWords = 0;
+        let mainArticle = '';
+
+        if ($('article').length > 0) {
+            const text = $('article').text();
+            mainArticle = text.trim()
+        } else {
+            $('body').find('*').each(function() {
+                const text = $(this).text();
+                const numberWords = text.trim().split(/\s+/).length;
+                if (numberWords > maxNumberWords) {
+                    maxNumberWords = numberWords;
+                    mainArticle = text
+                }
+            })
+        }
+
     
-        const summarizedText = await getSummarizedText(text);
+        const summarizedText = await getSummarizedText(mainArticle);
         return summarizedText
         
     } catch (error) {
+        console.error(error.response.data)
         throw new Error ("Error requesting web page.")
     }
 }
@@ -25,17 +43,14 @@ async function getSummarizedText(text: string): Promise<string> {
             const prompt = `Summarize the following text:\n\n${text}\n\nSummary:`;
 
             const requestBody= {
-                prompt: prompt,
-                model: "text-davinci-003",
-                max_tokens: 2000,
+                model: "gpt-3.5-turbo",
+                messages: [{"role": "user", "content": prompt}],
                 temperature: 0.2,
                 n: 1,
                 stream: false,
-                logprobs: null,
-                stop: "\n"
             }
 
-            const response = await axios.post('https://api.openai.com/v1/completions', requestBody, {
+            const response = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, {
                 headers: {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${apiKey}`
@@ -44,7 +59,7 @@ async function getSummarizedText(text: string): Promise<string> {
 
             const summaryResult = response.data;
 
-            return summaryResult.choices[0].text.trim() as string;
+            return summaryResult.choices[0].message.content.trim() as string;
 
         } catch (error) {
             console.error(error.response.data);
